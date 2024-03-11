2ab27aa76cf7cfc634a91881afc21ff5b7904209
Bryson Ramirez <142553045+Bryson00@users.noreply.github.com>
2024-03-11 17:13:16 +0000

    Mon, Mar 11, 2024, 10:13 AM -07:00

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,5 +1,16 @@
 toggle = document.getElementById('toggle')
+open = document.getElementById('open')
+modal = document.getElementById('modal')
+close = document.getElementById('close')
 
 toggle.addEventListener('click', () => {
-    document.body.classList.toggle
+    document.body.classList.toggle('show-nav')
+})
+
+open.addEventListener('click', () => {
+    modal.classList.add('show-modal')
+})
+
+close.addEventListener('click', () => {
+    modal.classList.remove('show-modal')
 })
